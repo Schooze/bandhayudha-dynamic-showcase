cb605bdd48b39c0448b3062c6316bf2dee83f42d
Schooze <eatmilky4@gmail.com>
2025-07-24 12:30:38 +0700

    Fixing EasyEDA Logo v2

--- src/components/SponsorsSection.tsx
+++ src/components/SponsorsSection.tsx
@@ -24,7 +24,7 @@ const SponsorsSection = () => {
     },
     {
       name: 'EasyEda',
-      logo: '/bandhayudha-photo/EasyEDA_Logo.png',
+      logo: '/bandhayudha-photo/EasyEDA_logo.png',
       description: 'Circuit Design'
     },
     {
