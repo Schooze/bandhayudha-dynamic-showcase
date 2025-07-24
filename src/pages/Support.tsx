94c3a36fdbb0a332a98e8a212fef5ac80b5e0ba2
Schooze <eatmilky4@gmail.com>
2025-07-24 21:12:22 +0700

    Support, Add background image to card v10

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -131,7 +131,7 @@ const Support = () => {
                     }}
                   >
                     {/* Overlay for better logo visibility */}
-                    <div className="absolute inset-0 bg-black/50"></div>
+                    <div className="absolute inset-0 bg-black/35"></div>
                     
                     {/* Logo in center with size constraints */}
                     <div className="absolute inset-0 flex items-center justify-center p-8">
